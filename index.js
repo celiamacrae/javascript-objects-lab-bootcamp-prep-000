e7deaec9484e05ff9343d760be2a8aa78a2f1448
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-09 15:32:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object.key = value
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
 }
